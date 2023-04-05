e0a16b89af24989f315ad0500d11c6db20b1f85e
Abhijit Barman <37418029+abhibarman@users.noreply.github.com>
2023-04-05 11:00:34 +0530

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -5,11 +5,11 @@ import numpy as np
 import pandas as pd
 from clearml import Task
 import pickle
-
+from numerize import numerize
 
 def get_dataframe(num_rows):
 
-    task = Task.get_task('7b30f2f252e04d15be209f68d23f4d18')
+    task = Task.get_task('d3bf17a06e49451bb82bf3352a271eea')
     args = {'steps' : num_rows}
     model_path  = task.artifacts['sarimax_model'].get_local_copy()
 
@@ -25,7 +25,7 @@ def get_dataframe(num_rows):
 st.title('Retail Store Sales Forecast')
 
 # Get user input
-num_rows = st.number_input('Forcast for next:', min_value=1, max_value=1000, value=14)
+num_rows = st.number_input('Forcast for next:', min_value=1, max_value=1000, value=28)
 
 # Generate the DataFrame
 df = get_dataframe(num_rows)
@@ -41,10 +41,15 @@ maximas = (np.diff(np.sign(np.diff(sales))) < 0).nonzero()[0] + 1
 fig, ax = plt.subplots()
 plt.plot(dates, sales)
 # Then mark the maximas and the minimas
-for i,minima in enumerate(minimas):
+for minima in minimas:
     plt.plot(df.iloc[minima]["timestamp"], df.iloc[minima]["predicted_mean"], marker="o")
-for i,maxima in enumerate(maximas):
+    y_label = numerize.numerize(df.iloc[minima]['predicted_mean'])
+    ax.text(df.iloc[minima]["timestamp"], df.iloc[minima]["predicted_mean"], f"{y_label}", size=12)
+    
+for maxima in maximas:
     plt.plot(df.iloc[maxima]["timestamp"], df.iloc[maxima]["predicted_mean"], marker="o")
+    y_label = numerize.numerize(df.iloc[maxima]['predicted_mean'])
+    ax.text(df.iloc[maxima]["timestamp"], df.iloc[maxima]["predicted_mean"], f"{y_label}", size=12)
 
 #plt.legend()
 ax.set_title(f'Sales for next {num_rows} Days')
